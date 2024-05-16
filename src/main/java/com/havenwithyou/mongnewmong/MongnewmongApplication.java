f57b435af1606c8069d1882e82b22318d9c22edc
jiheui <deveoper.jiheuilee@gmail.com>
2024-05-16 11:29:14 +0900

    initial commit

---
+++ src/main/java/com/havenwithyou/mongnewmong/MongnewmongApplication.java
@@ -0,0 +1,13 @@
+package com.havenwithyou.mongnewmong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongnewmongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongnewmongApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/havenwithyou/mongnewmong/MongnewmongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.havenwithyou.mongnewmong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongnewmongApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
